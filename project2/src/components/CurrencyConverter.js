2cff87f3a0ba36d788b85aabea22bb4f69dfcd12
Kot <mariikot06@gmail.com>
2024-08-16 10:59:34 +0300

    Update CurrencyConverter.js

--- project2/src/components/CurrencyConverter.js
+++ project2/src/components/CurrencyConverter.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import './CurrencyConverter.css'; // Импортируем CSS файл
+import './CurrencyConverter.css';
 
 const CurrencyConverter = () => {
   const [amount, setAmount] = useState('');
